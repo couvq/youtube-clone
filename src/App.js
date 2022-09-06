5dc3c35e08a872eeacaf85c88f3de6ebba7ac178
couvq <quentincouvelaire@gmail.com>
2022-09-06 13:49:04 -0400

    created components for our various routes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { Box } from '@mui/material';
+import NavBar from './components/NavBar';
+import Feed from './components/Feed';
+import VideoDetail from './components/VideoDetail';
+import ChannelDetail from './components/ChannelDetail';
+import SearchFeed from './components/SearchFeed';
 
 const App = () => {
 
